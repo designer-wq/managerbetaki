f46478f829a766ff8b402a1a88361ed46a5cb829
Felipe Mota <fmota@Felipes-MacBook-Air.local>
2025-12-14 16:05:56 -0300

    Fix undefined supabase variables

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_URL_KEY = 'supabase_url';
 const SUPABASE_KEY_KEY = 'supabase_key';
 
-
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 let supabaseInstance: ReturnType<typeof createClient> | null = null;
 
 export const getSupabase = () => {
