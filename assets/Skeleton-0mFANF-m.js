30923ded2f2f523780c85614247ca706ab3cccb0
fmota2701 <99359128+fmota2701@users.noreply.github.com>
2026-01-08 11:26:36 -0300

    Updates

---
+++ assets/Skeleton-0mFANF-m.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-CV6oCzd2.js";function o({className:e,...n}){return s.jsx("div",{className:`animate-pulse rounded-md bg-zinc-700/50 ${e}`,...n})}export{o as S};
---
+++ assets/trending-up-4FqNLMJ8.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-CV6oCzd2.js";/**
+ * @license lucide-react v0.561.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20",key:"13o1zl"}],["path",{d:"M2 12h20",key:"9i4pu4"}]],n=e("globe",o);/**
+ * @license lucide-react v0.561.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const c=[["path",{d:"M16 7h6v6",key:"box55l"}],["path",{d:"m22 7-8.5 8.5-5-5L2 17",key:"1t1m79"}]],a=e("trending-up",c);export{n as G,a as T};
